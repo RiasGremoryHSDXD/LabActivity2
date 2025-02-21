6e9eca31206ef135ee0b375a97d4389697e01fb5
Rias Gremory <153193656+RiasGremoryHSDXD@users.noreply.github.com>
2025-02-21 18:50:01 +0800

    Button container for column 1 is completed

---
+++ js/main_first_column.js
@@ -0,0 +1,38 @@
+let get_my_group = document.getElementById('my_group')
+let get_my_group_clicked = document.getElementById('my_group_clicked')
+
+let get_my_event = document.getElementById('my_event')
+let get_my_event_clicked = document.getElementById('my_event_clicked')
+
+let get_my_photos = document.getElementById('my_photos')
+let get_my_photos_clicked = document.getElementById('my_photos_clicked')
+
+get_my_group.addEventListener('click', () => {
+    if (getComputedStyle(get_my_group_clicked).display === "none") {
+        get_my_group_clicked.style.display = "block";
+    } else {
+        get_my_group_clicked.style.display = "none";
+    }
+})
+
+get_my_event.addEventListener('click', () =>{
+    if(getComputedStyle(get_my_event_clicked).display === "none")
+    {
+        get_my_event_clicked.style.display = "block"
+    }
+    else
+    {
+        get_my_event_clicked.style.display = "none"
+    }
+})
+
+get_my_photos.addEventListener('click', () => {
+    if(getComputedStyle(get_my_photos_clicked).display === "none")
+    {
+        get_my_photos_clicked.style.display = "block"
+    }
+    else
+    {
+        get_my_photos_clicked.style.display = "none"
+    }
+})
